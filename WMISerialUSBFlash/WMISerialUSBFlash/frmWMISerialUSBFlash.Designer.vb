77db5ba6233a1fe1f948b537577b3046a273ace9
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-08-15 18:18:45 +0700

    Add files via upload

---
+++ WMISerialUSBFlash/WMISerialUSBFlash/frmWMISerialUSBFlash.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWMISerialUSBFlash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvwData = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'lvwData
+        '
+        Me.lvwData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lvwData.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lvwData.Location = New System.Drawing.Point(0, 0)
+        Me.lvwData.Name = "lvwData"
+        Me.lvwData.Size = New System.Drawing.Size(667, 304)
+        Me.lvwData.TabIndex = 2
+        Me.lvwData.UseCompatibleStateImageBehavior = False
+        '
+        'frmWMISerialUSBFlash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(667, 304)
+        Me.Controls.Add(Me.lvwData)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmWMISerialUSBFlash"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WMI Serial USB Flash Drive with Added or Removed Event. (Auto Detect)"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lvwData As System.Windows.Forms.ListView
+
+End Class
---
+++ WMISerialUSBFlash/WMISerialUSBFlash/frmWMISerialUSBFlash.vb
@@ -0,0 +1,173 @@
+﻿' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: http://www.facebook.com/g2gnet (For Thailand only)
+' / Facebook: http://www.facebook.com/CommonIndy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' / Co-Developer: https://www.facebook.com/apirak.wongwai.5
+' /
+' / Purpose: Auto Detect and get Serial Number of USB Flash Drive with WMI (Windows Management Instrumentation).
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+
+'// Don't forget to Add Reference --> System.Management
+Imports System.Management
+Imports Microsoft.Win32
+Imports System.Threading
+
+Public Class frmWMISerialUSBFlash
+    '// Win32_VolumeChangeEvent ::  https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-volumechangeevent
+    Dim WithEvents pluggedInWatcher As ManagementEventWatcher
+    Dim WithEvents pluggedOutWatcher As ManagementEventWatcher
+    Dim pluggedInQuery As WqlEventQuery
+    Dim pluggedOutQuery As WqlEventQuery
+    Dim currentDrive As String = ""
+
+    Private Sub frmWMISerialUSBFlash_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ''// ByPass Cross-thread operation.
+        System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+        ' Initialize ListView Control
+        With lvwData
+            .View = View.Details
+            .GridLines = True
+            .FullRowSelect = True
+            .HideSelection = False
+            .MultiSelect = False
+            .Columns.Add("Drive", lvwData.Width \ 3 - 170)
+            .Columns.Add("Model", lvwData.Width \ 3 + 115)
+            .Columns.Add("Serial Number", lvwData.Width \ 3 + 50)
+        End With
+
+        ''// สร้างตัวตรวจจับการเสียบ/ถอด USB (EventType 1 = Configuration Changed, 2 = Device Arrival, 3 = Device Removal, 4 = Docking)
+        Try
+            ''// เริ่มตรวจจับการเสียบ USB
+            pluggedInQuery = New WqlEventQuery
+            pluggedInQuery.QueryString = "SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2"
+            pluggedInWatcher = New ManagementEventWatcher(pluggedInQuery)
+            pluggedInWatcher.Start()
+
+            ''// เริ่มตรวจจับการถอด USB
+            pluggedOutQuery = New WqlEventQuery
+            pluggedOutQuery.QueryString = "SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 3"
+            pluggedOutWatcher = New ManagementEventWatcher(pluggedOutQuery)
+            pluggedOutWatcher.Start()
+
+            '// ลูปหา USB Flash Drive ทั้งหมดที่กำลังถูกเสียบคาเครื่องอยู่
+            Call ReadUsbFlashSerialNo()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub pluggedInWatcher_EventArrived(sender As Object, e As EventArrivedEventArgs) Handles pluggedInWatcher.EventArrived
+        currentDrive = e.NewEvent.Properties("DriveName").Value.ToString
+
+        '// ลูปหา USB Flash Drive ทั้งหมดที่กำลังถูกเสียบคาเครื่องอีกครั้ง
+        Call ReadUsbFlashSerialNo()
+    End Sub
+
+    Private Sub pluggedOutWatcher_EventArrived(sender As Object, e As EventArrivedEventArgs) Handles pluggedOutWatcher.EventArrived
+        currentDrive = e.NewEvent.Properties("DriveName").Value.ToString
+
+        '// ลูปหา USB Flash Drive ทั้งหมดที่กำลังถูกเสียบคาเครื่องอีกครั้ง
+        Call ReadUsbFlashSerialNo()
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / Procedure to read USB Flash Serial Number (Physical Serial Number , Not Volume) and return value all drives.
+    Private Sub ReadUsbFlashSerialNo()
+        Dim strTemp As String = ""
+        Dim strArr() As String      '<-- คำตอบสุดท้าย (Physical Serial Number นั่นเอง)
+        Dim MaxLen As Byte = 0      '<-- หาค่าความยาวสูงสุดของชุดข้อมูล ซึ่งจะเป็น Serial Number
+        Dim idx As Byte = 0         '<-- เป็น Index ของ Array ซึ่งจะเก็บค่าคำตอบ Serial Number
+        '// Start
+        Dim thread As Thread = New Thread(Sub()
+                                              Try
+                                                  '// สร้าง ListViewItem
+                                                  Dim LV As ListViewItem
+
+                                                  '// Windows Query Language หรือ WQL "Win32_DiskDrive"
+                                                  '// เลือกมาเฉพาะ InterfaceType = 'USB' และ MediaType = 'Removable Media'
+                                                  '// เหตุผลที่เลือก MediaType = 'Removable Media' เพราะบางเครื่องอาจมี Card Reader อยู่
+                                                  Dim WmiQuery As String = "SELECT Name,Model,PnPDeviceID FROM Win32_DiskDrive WHERE InterfaceType = 'USB' And MediaType = 'Removable Media'"
+                                                  Dim WmiSearcher As New ManagementObjectSearcher(WmiQuery)
+
+                                                  '// เคลียร์ค่าใน ListView
+                                                  lvwData.Items.Clear()
+
+                                                  '// ลูปหา USB Flash Drive ทั้งหมดที่กำลังถูกเสียบคาเครื่อง
+                                                  For Each info As ManagementObject In WmiSearcher.Get()
+                                                      '// รับค่า USB Storage จากการอ่านของ WMI
+                                                      ' "USBSTOR\DISK&VEN_&PROD_&REV_1.00\7&15FF80F3&0&__&0"
+                                                      strTemp = (String.Format("{0}", info("PnPDeviceID")))
+                                                      '// แยกชุดข้อมูลออกจากกันด้วยเครื่องหมาย \
+                                                      strArr = strTemp.Split("\")
+                                                      ' strArr(0) = "USBSTOR"
+                                                      ' strArr(1) = "DISK&VEN_&PROD_&REV_1.00"
+                                                      ' strArr(2) = "7&15FF80F3&0&__&0"
+                                                      '// เลือกเอาข้อมูลชุดสุดท้ายที่มี UBound สูงสุด จะได้ "7&15FF80F3&0&__&0" ซึ่งเป็นคำตอบแต่ยังไม่สุดท้าย
+                                                      strTemp = strArr(UBound(strArr))
+
+                                                      strArr = New String() {} ' implicit size, initialized to empty
+                                                      '// แยก "7&15FF80F3&0&__&0" ด้วยเครื่องหมาย &
+                                                      strArr = strTemp.Split("&")
+                                                      ' strArr(0) = "7"               '<-- LBound
+                                                      ' strArr(1) = "15FF80F3"
+                                                      ' strArr(2) = "0"
+                                                      ' strArr(3) = "__"
+                                                      ' strArr(4) = "0"               '<-- UBound
+                                                      '// ลูปตามจำนวน Index ของ Array โดยเริ่มจาก LBound = 0 ไปจนสิ้นสุดที่ UBound
+                                                      '// เพื่อหาช่องเก็บข้อมูลยาวที่สุด
+                                                      For Count = LBound(strArr) To UBound(strArr)
+                                                          '// เปรียบเทียบค่าความยาวของข้อมูลในแต่ละ Array
+                                                          If MaxLen < Len(strArr(Count)) Then
+                                                              '// เก็บค่า Index ไว้เป็นคำตอบ (ตัวอย่างคือ Index = 1)
+                                                              idx = Count
+                                                              '// นำค่าความยาวที่มากกว่ามาเก็บไว้ ก่อนที่วนกลับไปเปรียบเทียบค่าใหม่
+                                                              MaxLen = Len(strArr(Count))
+                                                          End If
+                                                      Next
+                                                      '// นำคำตอบสุดท้ายมาแสดงผลใน ListView
+                                                      '// Drive Letter
+                                                      LV = lvwData.Items.Add(GetDriveLetterFromDisk(String.Format("{0}", info("Name"))))
+                                                      '// Model
+                                                      LV.SubItems.Add(Trim(info("Model")))
+                                                      '// Physical Serial Number
+                                                      LV.SubItems.Add(Replace(Trim(strArr(idx)), Chr(0), ""))
+                                                  Next
+                                              Catch err As ManagementException
+                                                  MessageBox.Show("An error occurred while querying for WMI data: " & err.Message)
+                                              End Try
+                                          End Sub)
+        thread.Start()
+        thread.Join()
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / Function to read Usb Flash Drive Letter.
+    Public Function GetDriveLetterFromDisk(ByVal Name As String) As String
+        Dim qPart, qDisk As ObjectQuery
+        Dim mPart, mDisk As ManagementObjectSearcher
+        Dim objPart, objDisk As ManagementObject
+        Dim DriveLetter As String = ""
+        ' WMI queries use the "\" as an escape charcter
+        Name = Replace(Name, "\", "\\")
+        ' First we map the Win32_DiskDrive instance with the association called
+        ' Win32_DiskDriveToDiskPartition. Then we map the Win23_DiskPartion
+        ' instance with the assocation called Win32_LogicalDiskToPartition
+        qPart = New ObjectQuery("ASSOCIATORS OF {Win32_DiskDrive.DeviceID=""" & Name & """} WHERE AssocClass = Win32_DiskDriveToDiskPartition")
+        mPart = New ManagementObjectSearcher(qPart)
+        For Each objPart In mPart.Get()
+            qDisk = New ObjectQuery("ASSOCIATORS OF {Win32_DiskPartition.DeviceID=""" & objPart("DeviceID").ToString & """} WHERE AssocClass = Win32_LogicalDiskToPartition")
+            mDisk = New ManagementObjectSearcher(qDisk)
+            For Each objDisk In mDisk.Get()
+                DriveLetter &= objDisk("Name").ToString
+            Next
+        Next
+        Return DriveLetter
+    End Function
+End Class
